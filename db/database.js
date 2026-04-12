611fee36bb3b079ce4ff6a665cf6e2c84db7f898
GALLIE Koffi Yann-Armel <professional.gallie@gmail.com>
2026-04-12 23:30:56 +0000

    Modification de database.js pour faciliter son déploiement sur railway

--- db/database.js
+++ db/database.js
@@ -1,14 +1,9 @@
 import pkg from 'pg';
 const { Pool } = pkg;
-import dotenv from 'dotenv';
-dotenv.config({ path: './env/.env' });
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false } // requis pour Railway
 });
-
-pool.connect()
-  .then(() => console.log('PostgreSQL connecté !'))
-  .catch(err => console.error('Erreur connexion DB :', err));
 
 export default pool;
