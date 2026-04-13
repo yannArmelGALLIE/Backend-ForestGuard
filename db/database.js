8ae8d9710a9124d2c780da3b36f5c658cd219433
GALLIE Koffi Yann-Armel <professional.gallie@gmail.com>
2026-04-13 00:18:00 +0000

    Dernières modif

--- db/database.js
+++ db/database.js
@@ -1,9 +1,15 @@
 import pkg from 'pg';
 const { Pool } = pkg;
+import dotenv from 'dotenv';
+dotenv.config({ path: './env/.env' });
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false } // requis pour Railway
+  ssl: { rejectUnauthorized: false } // ← obligatoire pour Railway
 });
+
+pool.connect()
+  .then(() => console.log('PostgreSQL Railway connecté !'))
+  .catch(err => console.error('Erreur connexion DB :', err));
 
 export default pool;
